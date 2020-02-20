eaa52f32582e1747753fc59915f87e53896b4639
ynonp <ynonperek@gmail.com>
2020-02-20 16:15:39 +0200

    before mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+interface IItem {
+  id: number;
+  name: string;
+  done: boolean;
+}
+
+function NewItemBox({addItem}: { addItem: (_:string) => void}) {
+    const [ text, setText ] = useState('');
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+        addItem(text);
+        setText('');
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+          <input
+              type={"text"}
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+          />
+            <button type={"submit"}>Add</button>
+        </form>
+    )
+}
+
+
+function Item({ item, toggleItem }: {
+    item: IItem,
+    toggleItem: (itemId: number) => void,
+}) {
+    return (
+        <li>
+            <label>
+                <input
+                    type={"checkbox"}
+                    checked={item.done}
+                    onChange={() => toggleItem(item.id)} />
+                {item.name}
+            </label>
+
+
+        </li>
+    )
+}
+
+function ItemsList({ items, toggleItem }: {
+    items: IItem[] ,
+    toggleItem: (itemId: number) => void,
+}) {
+    return (
+        <ul>
+            {items.map(item => (
+                <Item item={item} toggleItem={toggleItem}/>
+            ))}
+        </ul>
+    )
+}
+
+function RemainingItems({ items }: { items: IItem [] }) {
+    const remaining = items.filter(item => !item.done).length;
+    return (
+        <p>You have {remaining} items left to do</p>
+    )
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, setItems] = useState<IItem []>([]);
+
+    function addItem(name: string) {
+        const newItem = {
+            id: items.length,
+            name,
+            done: false,
+        }
+        setItems([...items, newItem]);
+    }
+
+    function toggleItem(id: number) {
+        setItems(items.map(item => (
+            item.id === id ? {...item, done: !item.done} : item
+        )));
+    }
+
+    return (
+        <div className="App">
+            <NewItemBox addItem={addItem}/>
+            <ItemsList items={items} toggleItem={toggleItem} />
+            <RemainingItems items={items} />
+        </div>
+    );
 }
 
 export default App;
